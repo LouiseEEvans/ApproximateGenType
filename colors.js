82fcb398292a459a33d1d84219b18674310e3551
louloudo <louise-e-evans@hotmail.com>
2017-01-05 14:59:57 +0000

    Initial commit

---
+++ colors.js
@@ -0,0 +1,17 @@
+var palette1 = [
+  '#00ffde',
+  '#ec0030',
+  '#01c49c',
+  '#bdd4cf',
+  '#fcd001',
+  'black'
+];
+
+var palette2 = [
+  '#00ffeb',
+  '#ea7c6d',
+  '#161ffd',
+  '#dfd9de',
+  '#7e7d9d',
+  'white'
+];
